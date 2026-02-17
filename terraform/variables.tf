33bb34b4560f252a2823ee634124bb04a8a902cc
Prashant <thedevopsprashant@gmail.com>
2026-02-17 19:46:34 +0530

    updated tf configs provider etc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -37,17 +37,17 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.retail_app_eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.retail_app_eks.cluster_name]
-    }
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.retail_app_eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "aws"
+#       args        = ["eks", "get-token", "--cluster-name", module.retail_app_eks.cluster_name]
+#     }
+#   }
+# }
 
 provider "kubectl" {
   host                   = module.retail_app_eks.cluster_endpoint
