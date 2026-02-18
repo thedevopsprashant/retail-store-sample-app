7af97a2197b148f2b298f7d947f3fa0c970ef39d
Prashant <thedevopsprashant@gmail.com>
2026-02-18 13:03:44 +0530

    updated argocd urls

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,7 +41,7 @@ variable "argocd_namespace" {
 variable "argocd_chart_version" {
   description = "ArgoCD Helm chart version"
   type        = string
-  default     = "5.51.6"
+  default     = "9.4.2"
 }
 
 variable "enable_single_nat_gateway" {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -26,6 +26,10 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
   }
 }
 
@@ -48,6 +52,17 @@ provider "aws" {
 #     }
 #   }
 # }
+
+provider "kubernetes" {
+    host                   = module.retail_app_eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.retail_app_eks.cluster_name]
+    }
+  }
+
 
 provider "kubectl" {
   host                   = module.retail_app_eks.cluster_endpoint
