22d555d5999a0b1aa55a47ae7578d279803a2cd2
Prashant <thedevopsprashant@gmail.com>
2026-04-05 11:50:20 +0530

    added backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -31,6 +31,14 @@ terraform {
       version = ">= 3.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "prashant-tfstates-bucket"
+    region       = "ap-south-1"
+    key          = "retail-app/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
 # =============================================================================
